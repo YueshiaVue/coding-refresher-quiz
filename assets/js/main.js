e9089db23c849fe6a50485835754c41a9837a76d
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-06-18 13:07:49 -0500

    Added functions and added another JavaScript file for the quiz

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,31 @@ function startQuiz() {
   timerEl.textContent = time;
 }
 
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+  }
+
+function getQuestion() {
+  var currentQuestion = questions[currentQuestionIndex];
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+  choicesEl.innerHTML = "";
+  currentQuestion.choices.forEach(function(choice, i) {
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+    choiceNode.textContent = i + 1 + ". " + choice;
+    choiceNode.onclick = questionClick;
+    choicesEl.appendChild(choiceNode);
+  });
+}
+
+function startQuiz() {
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "cloak");
+  questionsEl.removeAttribute("class");
+  timerId = setInterval(clockTick, 1000);
+  timerEl.textContent = time;
+  getQuestion();
+}
---
+++ assets/js/quiz.js
@@ -0,0 +1,8 @@
+var questions = [
+    {
+        title: 'What do you call listed variables when you define a function?',
+        choices: ['parameters', 'strings', 'booleans', 'arrays'],
+        answer : "parameters"
+    },
+
+];

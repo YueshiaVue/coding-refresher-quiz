111ff4d1b64d072dd6ef47986fa3495aeb233bc2
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-06-18 17:23:29 -0500

    Added more functions and another HTML file for the highscore

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
 var timerEl = document.getElementById('time');
@@ -10,27 +9,31 @@ var initialsEl = document.getElementById('initials');
 var responseEl = document.getElementById('response');
 
 function startQuiz() {
-  var startScreenEl = document.getElementById("start-screen");
-  startScreenEl.setAttribute("class", "cloak");
-  questionsEl.removeAttribute("class");
+  var startScreenEl = document.getElementById('start-screen');
+  startScreenEl.setAttribute('class', 'cloak');
+  questionsEl.removeAttribute('class');
   timerId = setInterval(clockTick, 1000);
   timerEl.textContent = time;
+  getQuestion();
 }
 
 function clockTick() {
     time--;
     timerEl.textContent = time;
+    if (time <= 0) {
+        endQuiz();
+      }
   }
 
 function getQuestion() {
-  var currentQuestion = questions[currentQuestionIndex];
-  var titleEl = document.getElementById("question-title");
+  var currentQuestion = questions[questionIdx];
+  var titleEl = document.getElementById('question-title');
   titleEl.textContent = currentQuestion.title;
-  choicesEl.innerHTML = "";
+  choicesEl.innerHTML = '';
   currentQuestion.choices.forEach(function(choice, i) {
-    var choiceNode = document.createElement("button");
-    choiceNode.setAttribute("class", "choice");
-    choiceNode.setAttribute("value", choice);
+    var choiceNode = document.createElement('button');
+    choiceNode.setAttribute('class', 'choice');
+    choiceNode.setAttribute('value', choice);
     choiceNode.textContent = i + 1 + ". " + choice;
     choiceNode.onclick = questionClick;
     choicesEl.appendChild(choiceNode);
@@ -38,10 +41,75 @@ function getQuestion() {
 }
 
 function startQuiz() {
-  var startScreenEl = document.getElementById("start-screen");
-  startScreenEl.setAttribute("class", "cloak");
-  questionsEl.removeAttribute("class");
+  var startScreenEl = document.getElementById('start-screen');
+  startScreenEl.setAttribute('class', 'cloak');
+  questionsEl.removeAttribute('class');
   timerId = setInterval(clockTick, 1000);
   timerEl.textContent = time;
   getQuestion();
 }
+
+
+function questionClick() {
+    if (this.value !== questions[questionIdx].answer) {
+      time -= 10;
+
+      if (time < 0) {
+        time = 0;
+      }
+
+      timerEl.textContent = time;
+  
+      responseEl.textContent = 'Wrong!';
+    } else {
+
+      responseEl.textContent = 'Correct!';
+    }
+
+    responseEl.setAttribute('class', 'response');
+    setTimeout(function() {
+      responseEl.setAttribute('class', 'response cloak');
+    }, 1000);
+    currentQuestionIndex++;
+    if (currentQuestionIndex === questions.length) {
+      endQuiz();
+    } else {
+      getQuestion();
+    }
+  }
+
+function endQuiz() {
+    clearInterval(timerId);
+    var endPageEl = document.getElementById('end-page');
+    endPageEl.removeAttribute('class');
+    var finalScoreEl = document.getElementById('final-score');
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute('class', 'cloak');
+  }
+
+
+  function saveScore() {
+    var initials = initialsEl.value.trim();
+
+    if (initials !== "") {
+      
+      var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+      var newScore = {
+        score: time,
+        initials: initials
+      };
+      highscores.push(newScore);
+      window.localStorage.setItem('highscores', JSON.stringify(highscores));
+      window.location.href = 'highscores.html';
+    }
+  }
+  
+  function checkForEnter(event) {
+    if (event.key === 'Enter') {
+      saveScore();
+    }
+  }
+  
+  submitBtn.onclick = saveScore;
+  startBtn.onclick = startQuiz;
+  initialsEl.onkeyup = checkForEnter;
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,8 +1,38 @@
+var questionIdx = 0;
 var questions = [
     {
         title: 'What do you call listed variables when you define a function?',
-        choices: ['parameters', 'strings', 'booleans', 'arrays'],
-        answer : "parameters"
+        choices: ['Parameters', 'Strings', 'Booleans', 'Arrays'],
+        answer : 'Parameters'
     },
 
+    {
+        title: 'What is JSON?',
+        choices: ['JavaScript Object Notation', 'JavaScript Once Negated', 'JavaScript On Neigh', 'Javascript Only Negated'],
+        answer : 'JavaScript Object Notation'
+    },
+
+    {
+        title: 'What do you call a funtion that belongs to an object?',
+        choices: ['Array', 'Concat', 'Console.Log', 'Method'],
+        answer : 'Method'
+    },
+
+    {
+        title: 'How do you add a comment?',
+        choices: ['===', '//', '*', '!'],
+        answer : '//'
+    },
+
+    {
+        title: 'How do you declare a variable?',
+        choices: ['v HelloWorld', 'va HelloWorld', 'var HelloWorld', 'variable HelloWorld'],
+        answer : 'var HelloWorld'
+    },
+
+    {
+        title: 'Which operator is used to assign a variable',
+        choices: ['+', '-', '=', '>'],
+        answer : '='
+    },
 ];
